4caf5d165f5a2a6711d409a8330aaa0afc349362
vikas2dx@gmail.com <coolvikas123>
2017-04-07 22:49:19 +0530

    maintactivity comments

--- app/src/main/java/com/example/user/swipetabs/MainActivity.java
+++ app/src/main/java/com/example/user/swipetabs/MainActivity.java
@@ -15,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        setSupportActionBar(toolbar);   //toolbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //back icon
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
